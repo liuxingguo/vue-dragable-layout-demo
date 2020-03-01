a838f4ad82dfa18a2fda0964ee059480571da226
liuxingguo <liuxingguo@139.com>
2020-03-01 23:00:51 +0800

    改造插件，添加布局模板

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-# drag-layout
+<h1 align="center">vue-dragable-layout-demo</h1>
 
-## Project setup
-```
-npm install
-```
+基于vuejs的拖拽布局示例，支持位置拖拽、大小拖拽，根据业务自定义设置1:2、2:1、1:1:1等比例布局。前往[示例demo](https://liuxingguo.github.io/vue-dragable-layout-demo/)查看效果。
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## 注意
 
-### Compiles and minifies for production
-```
-npm run build
-```
+基于[vue-grid-layout](https://github.com/jbaysolutions/vue-grid-layout)改造实现，pull request [#407](https://github.com/jbaysolutions/vue-grid-layout/pull/407)暂时没被合并，临时打包插件直接放在项目里`node_modules/vue-grid-layout/dist/vue-grid-layout.common.js`
 
-### Lints and fixes files
-```
-npm run lint
-```
+vue-grid-layout改造版在[https://github.com/liuxingguo/vue-grid-layout/tree/develop](https://github.com/liuxingguo/vue-grid-layout/tree/develop), 执行`npm run build-lib`打包
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,41 @@
+<!-- @format -->
+
 <template>
   <div class="home">
     <div class="leftmenu">
+      <Divider>模板</Divider>
+      <RadioGroup v-model="currentTemplate" @on-change="templateChange">
+        <Radio label="1:2" style="margin:8px;" border></Radio>
+        <Radio label="2:1" style="margin:8px;" border></Radio>
+        <Radio label="1:1:1" style="margin:8px;" border></Radio>
+        <Radio label="自由" style="margin:8px;" border></Radio>
+      </RadioGroup>
+      <Divider>组件</Divider>
       <CheckboxGroup v-model="checkedItems" @on-change="checkBoxChange">
         <Checkbox
           v-for="item in items"
           :key="item.id"
           :label="item.label"
           border
+          style="margin:8px;"
         ></Checkbox>
       </CheckboxGroup>
       <Button type="primary" @click="saveLayout">保存布局</Button>
     </div>
     <div class="main">
       <grid-layout
+        class="divider"
+        :class="dividerStyle"
         :layout.sync="layout"
-        :col-num="12"
-        :row-height="30"
+        :col-num="colNum"
+        :row-height="rowHeight"
         :is-draggable="true"
         :is-resizable="true"
         :is-mirrored="false"
         :vertical-compact="true"
         :margin="[10, 10]"
         :use-css-transforms="true"
+        :drag-transform="dragTransform"
       >
         <grid-item
           v-for="item in layout"
@@ -31,6 +45,7 @@
           :h="item.h"
           :i="item.i"
           :key="item.i"
+          :resizeAxis="item.resizeAxis"
         >
           <Card :bordered="false" class="iframeCard">
             <p slot="title">{{ item.label }}</p>
@@ -58,6 +73,12 @@ export default {
   },
   data() {
     return {
+      // 布局列数
+      colNum: 12,
+      // 布局行高
+      rowHeight: 30,
+      // 选择的布局模板
+      currentTemplate: "1:2",
       // 基础模块数据
       items: [
         {
@@ -90,6 +111,19 @@ export default {
   created() {
     this.loadLayoutData();
   },
+  computed: {
+    dividerStyle() {
+      if (this.currentTemplate === "1:2") {
+        return "divider1-2";
+      } else if (this.currentTemplate === "2:1") {
+        return "divider2-1";
+      } else if (this.currentTemplate === "1:1:1") {
+        return "divider1-1-1";
+      } else {
+        return "";
+      }
+    }
+  },
   methods: {
     // 初始化加载布局数据
     loadLayoutData() {
@@ -98,18 +132,20 @@ export default {
         {
           x: 0,
           y: 0,
-          w: 6,
-          h: 7,
+          w: 4,
+          h: 6,
           i: 1,
+          resizeAxis: "y",
           label: "百度",
           url: "https://www.baidu.com/"
         },
         {
-          x: 0,
+          x: 4,
           y: 0,
-          w: 6,
-          h: 7,
+          w: 8,
+          h: 6,
           i: 2,
+          resizeAxis: "y",
           label: "新浪",
           url: "https://www.sina.com.cn/"
         }
@@ -133,17 +169,51 @@ export default {
       }
       this.checkedItemsChangedBefore = selected;
     },
+    // 模板变化时触发
+    templateChange() {
+      this.translateByTemplate();
+    },
     // 添加模块，用于选中项新增时动态添加布局。
     addLayout(item) {
-      this.layout.push({
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 7,
-        i: item.id,
-        label: item.label,
-        url: item.url
-      });
+      const template = this.currentTemplate;
+      switch (template) {
+        case "1:2":
+        case "1:1:1":
+          this.layout.push({
+            x: 0,
+            y: 0,
+            w: this.colNum / 3,
+            h: 6,
+            resizeAxis: "y",
+            i: item.id,
+            label: item.label,
+            url: item.url
+          });
+          break;
+        case "2:1":
+          this.layout.push({
+            x: 0,
+            y: 0,
+            w: this.colNum * (2 / 3),
+            h: 6,
+            resizeAxis: "y",
+            i: item.id,
+            label: item.label,
+            url: item.url
+          });
+          break;
+        default:
+          this.layout.push({
+            x: 0,
+            y: 0,
+            w: this.colNum / 3,
+            h: 6,
+            resizeAxis: "xy",
+            i: item.id,
+            label: item.label,
+            url: item.url
+          });
+      }
     },
     // 移除模块，用于选中项取消时删除布局。
     delLayout(item) {
@@ -151,30 +221,118 @@ export default {
         return v.i === item.id;
       });
     },
-    // 布局转化为选中项，用于初始化数据时选中项反显。
+    // 布局转化为选中项，用于初始化数据时左侧选中项反显。
     layoutToSelect() {
       this.checkedItems = _.map(this.layout, "label");
       this.checkedItemsChangedBefore = this.checkedItems;
     },
+    // 按模板转换当前布局，用于模板改变时调整现有布局
+    translateByTemplate() {
+      // 组件按行列排序
+      let sortedLayout = _.sortBy(this.layout, ["y", "x"]);
+      let template = this.currentTemplate;
+      sortedLayout.forEach((item, index) => {
+        switch (template) {
+          case "1:2":
+            item.x = (index % 2) * (this.colNum / 3);
+            item.y = Math.floor(index / 2);
+            item.w = (this.colNum / 3) * ((index % 2) + 1);
+            item.resizeAxis = "y";
+            break;
+          case "2:1":
+            item.x = (index % 2) * ((this.colNum / 3) * 2);
+            item.y = Math.floor(index / 2);
+            item.w = index % 2 === 0 ? (this.colNum / 3) * 2 : this.colNum / 3;
+            item.resizeAxis = "y";
+            break;
+          case "1:1:1":
+            item.x = (index % 3) * (this.colNum / 3);
+            item.y = Math.floor(index / 3);
+            item.w = this.colNum / 3;
+            item.resizeAxis = "y";
+            break;
+          default:
+            item.resizeAxis = "xy";
+        }
+      });
+      this.layout = sortedLayout;
+    },
+    // 拖拽时根据布局调整组件位置和大小
+    dragTransform(x, y, h, w) {
+      // template
+      switch (this.currentTemplate) {
+        case "1:2":
+          if (x < this.colNum / 3 / 2) {
+            x = 0;
+            w = this.colNum / 3;
+          } else {
+            x = this.colNum / 3;
+            w = (this.colNum * 2) / 3;
+          }
+          break;
+        case "2:1":
+          if (x < this.colNum / 3) {
+            x = 0;
+            w = (this.colNum * 2) / 3;
+          } else {
+            x = (this.colNum * 2) / 3;
+            w = this.colNum / 3;
+          }
+          break;
+        case "1:1:1":
+          if (x < this.colNum / 3 / 2) {
+            x = 0;
+          } else if (x < (this.colNum * 2) / 3) {
+            x = this.colNum / 3;
+          } else {
+            x = (this.colNum * 2) / 3;
+          }
+          break;
+        default:
+          break;
+      }
+      return { x, y, h, w };
+    },
     // 保存布局
     saveLayout() {
-      // 调用后台接口保存布局
+      alert("打开控制台查看！");
+      // 调用后台接口保存布局 全部的文本保存起来，加载的时候再转json使用
       const layout = JSON.stringify(this.layout);
-      alert("保存的布局为：" + layout);
-      let layoutForPortal = this.layout.map(v => {
-        v.leftOffset = (v.x / 12) * 100 + "%";
-        v.topOffset = v.y * 30 + "px";
-        v.width = (v.w / 12) * 100 + "%";
-        v.height = v.h * 30 + "px";
-        return v;
-      });
-      alert("输出给门户的布局信息为:" + JSON.stringify(layoutForPortal));
+      window.console.log(layout);
+      // 输出给门户使用的布局数据根据x、y、w、h计算，注意组件间的间距为10px要计算在内。
+      // 比如行高为30px, h = 12, 则高度为：30*12+10*(12-1)
     }
   }
 };
 </script>
 
 <style scoped>
+.divider::before,
+.divider::after {
+  content: "";
+  display: none;
+  position: absolute;
+  width: 0;
+  top: 0;
+  bottom: 0;
+  border-left: 1px dashed #666;
+}
+.divider1-2::before {
+  display: block;
+  left: 33.333%;
+}
+.divider2-1::before {
+  display: block;
+  left: 66.667%;
+}
+.divider1-1-1::before {
+  display: block;
+  left: 33.333%;
+}
+.divider1-1-1::after {
+  display: block;
+  right: 33.333%;
+}
 .home {
   width: 100vw;
   height: 100vh;
@@ -187,9 +345,9 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
+  text-align: center;
 }
 .main {
-  min-height: 300px;
   margin: 10px 10px 10px 100px;
   border: 1px dashed #666;
   padding: 10px;
